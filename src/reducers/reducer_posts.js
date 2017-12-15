37fac9fd28c1b5718353c9f2729604bcbbbbc03b
csergey8 <c.sergey@protonmail.com>
2017-12-15 12:45:17 +0200

    add delete reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, CREATE_POST, FETCH_POST } from '../actions';
+import { FETCH_POSTS, CREATE_POST, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
@@ -12,6 +12,8 @@ export default function(state = {}, action) {
       return { ...state, [action.payload.data.id]: action.payload.data }
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     default:
       return state;
 
