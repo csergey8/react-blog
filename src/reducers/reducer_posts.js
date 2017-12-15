6070177265e83fb3638ba1450757bf43bc76985e
csergey8 <c.sergey@protonmail.com>
2017-12-15 10:58:55 +0200

    complete show post component

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,11 +4,12 @@ import _ from 'lodash';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POST:
+    console.log(action.payload.data.id);
       //const post = action.payload.data;
       //const newState = { ...state };
       //newState[post.id] = post;
       //return newState;
-      return { ...state, [action.payload.data.id]: action.payload.data}
+      return { ...state, [action.payload.data.id]: action.payload.data }
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     default:
